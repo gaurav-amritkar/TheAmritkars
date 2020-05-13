913b984d6bc037f7d3652507010647a2867e9bf8
gaurav-amritkar <gaurav.amritkar@gmail.com>
2020-05-13 16:34:11 +0530

    Adding a tree module

--- src/app/Content/Content/Home/Home.module.ts
+++ src/app/Content/Content/Home/Home.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from 'src/material.module';
 import { HomeComponent } from './Home.component';
 import { HomeRoutingModule } from './Home-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModule,
     HomeRoutingModule
   ],
   declarations: [
