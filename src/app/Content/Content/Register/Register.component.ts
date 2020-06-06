c0b97a109a44779e7429729d1dcf2406806b82b9
gaurav-amritkar <gaurav.amritkar@gmail.com>
2020-06-07 01:18:26 +0530

    Revert "Merge branch 'master' into gaurav"
    This reverts commit 51be872a84e883eda644293cc8e99b770d17a02f, reversing
    changes made to 666fe3bfee2405fd61c0122b4de2eb6746b0f15b.

--- src/app/Content/Content/Register/Register.component.ts
+++ src/app/Content/Content/Register/Register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-Register',
@@ -7,8 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  phoneNumber: any;
+  constructor(
+    private angularFireAuth: AngularFireAuth,
+    private firestore: AngularFirestore,
+    private snackBar: MatSnackBar,
+    private router: Router) {
+      this.angularFireAuth.user.subscribe(res => {
+        if (res === null) {
+          window.location.assign('/login');
+          this.snackBar.open('User is not Authenticated', 'Dismiss', { duration: 2000 });
+        } else {
+          this.phoneNumber = res.phoneNumber;
+        }
+      });
+  }
 
   ngOnInit() {}
+
+  AddUser() {
+    console.log('In add User func');
+
+    const item = {
+      first_name: 'Firebase',
+      last_name: 'User',
+      mobile_number: this.phoneNumber,
+      dummy: 'this does not exist already'
+    };
+    // Creates a document by provided name in the given collection and sets the data into it
+    return this.firestore.collection('users').doc('user_00000').set(item);
+  }
+
+  DeleteUser() {
+    console.log('In delete User func');
+
+    // Deletes a document by provided name in the given collection
+    return this.firestore.collection('users').doc('user_00000').delete();
+  }
 
 }
