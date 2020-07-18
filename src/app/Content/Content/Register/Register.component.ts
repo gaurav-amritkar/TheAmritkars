527549d4fdb4d22c9604e3fa75cd8c645f954149
gaurav-amritkar <gaurav.amritkar@gmail.com>
2020-07-18 19:51:43 +0530

    Added fields to register page

--- src/app/Content/Content/Register/Register.component.ts
+++ src/app/Content/Content/Register/Register.component.ts
@@ -11,6 +11,8 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
+  firstName: any;
+  lastName: any;
   phoneNumber: any;
 
   //
@@ -45,8 +47,8 @@ export class RegisterComponent implements OnInit {
     console.log('In add User func');
 
     const item = {
-      first_name: 'Firebase',
-      last_name: 'User',
+      first_name: this.firstName,
+      last_name: this.lastName,
       mobile_number: this.phoneNumber,
       dummy: 'this does not exist already'
     };
@@ -61,66 +63,66 @@ export class RegisterComponent implements OnInit {
     return this.firestore.collection('users').doc('user_00000').delete();
   }
 
-  submitPhoneNumberAuth() {
-    if (this.phoneNumber.length < 10) {
-      return;
-    }
-    const phoneNumber = '+91' + this.phoneNumber;
-    const appVerifier = this.app;
-    this.angularFireAuth.auth.signInWithPhoneNumber(phoneNumber, appVerifier)
-      .then(confirmationResult => {
-        this.confirmRe = confirmationResult;
-        this.mobileField = false;
-        this.otpField = true;
-      })
-      .catch(error => {
-        this.snackBar.open('Message Not Send', 'Dismiss', { duration: 3000 });
-        console.log('SMS not Send', error);
-      });
-  }
+  // submitPhoneNumberAuth() {
+  //   if (this.phoneNumber.length < 10) {
+  //     return;
+  //   }
+  //   const phoneNumber = '+91' + this.phoneNumber;
+  //   const appVerifier = this.app;
+  //   this.angularFireAuth.auth.signInWithPhoneNumber(phoneNumber, appVerifier)
+  //     .then(confirmationResult => {
+  //       this.confirmRe = confirmationResult;
+  //       this.mobileField = false;
+  //       this.otpField = true;
+  //     })
+  //     .catch(error => {
+  //       this.snackBar.open('Message Not Send', 'Dismiss', { duration: 3000 });
+  //       console.log('SMS not Send', error);
+  //     });
+  // }
 
-  SubmitPhoneNumberAuthCode() {
-    const cod = this.code;
-    this.confirmRe.confirm(cod)
-      .then(result => {
-        this.otpField = true;
-        this.mobileField = false;
-        const use = result.user;
-        if (use != null) {
-          console.log(use.displayName);
-          console.log(use.email);
-          console.log(use.photoURL);
-          console.log(use.emailVerified);
-          console.log(use.uid);
-          console.log('User Token', use.refreshToken);
-          console.log('MetaData', use.metadata.lastSignInTime);
-          // The user's ID, unique to the Firebase project. Do NOT use
-          // this value to authenticate with your backend server, if
-          // you have one. Use User.getToken() instead.
-        }
-        // if (use != null) {
-        //   use.providerData.forEach(profile => {
-        //     console.log('Sign-in provider: ' + profile.providerId);
-        //     console.log('Provider-specific UID: ' + profile.uid);
-        //     console.log('Name: ' + profile.displayName);
-        //     console.log('Email: ' + profile.email);
-        //     console.log('Photo URL: ' + profile.photoURL);
-        //   });
-        // }
-        // console.log('User', use);
-        this.angularFireAuth.auth.onAuthStateChanged(user => {
-          if (user) {
-            console.log('USER LOGGED IN');
-            this.snackBar.open('User Logged In Successfully', 'Dismiss', { duration: 3000 });
-          } else {
-            console.log('USER NOT LOGGED IN');
-          }
-        });
-      })
-      .catch(error => {
-        this.snackBar.open('Bad Verification Code', 'Dismiss', { duration: 3000 });
-        console.log('Bad Verification Code', error);
-      });
-  }
+  // SubmitPhoneNumberAuthCode() {
+  //   const cod = this.code;
+  //   this.confirmRe.confirm(cod)
+  //     .then(result => {
+  //       this.otpField = true;
+  //       this.mobileField = false;
+  //       const use = result.user;
+  //       if (use != null) {
+  //         console.log(use.displayName);
+  //         console.log(use.email);
+  //         console.log(use.photoURL);
+  //         console.log(use.emailVerified);
+  //         console.log(use.uid);
+  //         console.log('User Token', use.refreshToken);
+  //         console.log('MetaData', use.metadata.lastSignInTime);
+  //         // The user's ID, unique to the Firebase project. Do NOT use
+  //         // this value to authenticate with your backend server, if
+  //         // you have one. Use User.getToken() instead.
+  //       }
+  //       // if (use != null) {
+  //       //   use.providerData.forEach(profile => {
+  //       //     console.log('Sign-in provider: ' + profile.providerId);
+  //       //     console.log('Provider-specific UID: ' + profile.uid);
+  //       //     console.log('Name: ' + profile.displayName);
+  //       //     console.log('Email: ' + profile.email);
+  //       //     console.log('Photo URL: ' + profile.photoURL);
+  //       //   });
+  //       // }
+  //       // console.log('User', use);
+  //       this.angularFireAuth.auth.onAuthStateChanged(user => {
+  //         if (user) {
+  //           console.log('USER LOGGED IN');
+  //           this.snackBar.open('User Logged In Successfully', 'Dismiss', { duration: 3000 });
+  //         } else {
+  //           console.log('USER NOT LOGGED IN');
+  //         }
+  //       });
+  //     })
+  //     .catch(error => {
+  //       this.snackBar.open('Bad Verification Code', 'Dismiss', { duration: 3000 });
+  //       console.log('Bad Verification Code', error);
+  //     });
+  // }
 
 }
